4b91cd8e688632e8cc3bcf4e922374969acf60c8
Ben <manglebot@users.noreply.github.com>
2023-12-09 19:54:02 +0000

    fix:impliment useEffect

--- src/components/hooks/use_effect/use_effect.tsx
+++ src/components/hooks/use_effect/use_effect.tsx
@@ -1,13 +1,32 @@
-// make an api call using useEffect
+import { useEffect, useState } from "react";
+
+interface Todo {
+  title: string;
+}
 
 export const APICall = () => {
+  const [json, setJson] = useState<Todo | null>(null);
 
-    // Your code here!
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(
+          "https://jsonplaceholder.typicode.com/todos/1"
+        );
+        const data = await response.json();
+        setJson(data);
+      } catch (error) {
+        console.error("error fetching data:", error);
+      }
+    };
 
-    return (
-        <>
-            <h2>useEffect</h2>
-            <p>Your todo here!</p>
-        </>
-    )
-}
+    fetchData();
+  }, []);
+
+  return (
+    <>
+      <h2>useEffect</h2>
+      <p>{json ? json.title : "Loading..."}</p>
+    </>
+  );
+};
